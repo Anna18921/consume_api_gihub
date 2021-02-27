57bc6d21ecf4dc1c7e0d3ff798cfd943227d473a
rafaela <rafabispo53@gmail.com>
2021-02-27 03:04:48 -0300

    add index for export files

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import CardRepos from './CardRepos';
+import CardUser from './CardUser';
+import Search from './Search';
+import Footer from './Footer';
+
+export { CardRepos, CardUser, Search, Footer };
